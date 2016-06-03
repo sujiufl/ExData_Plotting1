adcbefeba99852cc2a7d975340162da281b29c72
sujiufl <sujiufl@yahoo.com>
2016-06-03 15:33:53 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,7 @@
 # Plot1.R 
+# See file Week1Assignment.R to see how the data was downloaded and read
+
+# create and plot the relevant plot1.png
 png("plot1.png", width = 480, height = 480)
 hist(as.numeric(subsetdata$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
